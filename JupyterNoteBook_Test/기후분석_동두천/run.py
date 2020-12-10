cf548e427d7847985c5f3c07dcee8819a99376bd
sidcode <sidcode@gmail.com>
2020-12-10 12:32:55 +0900

    이런저런 연습

--- JupyterNoteBook_Test/기후분석_동두천/run.py
+++ JupyterNoteBook_Test/기후분석_동두천/run.py
@@ -1,11 +1,26 @@
 import csv
+import matplotlib.pyplot as plt
 
-f = open('ta_20201208150322.csv', 'r', encoding='cp949') # csv 파일을 읽기전용으로 인코딩은 cp949 형식으로
+f = open('seoul.csv', 'r', encoding='cp949') # csv 파일을 읽기전용으로 인코딩은 cp949 형식으로
 data = csv.reader(f, delimiter=',')                      # csv를 ',' 구분자로 읽어와 data 변수에 치환
+header = next(data)
 
-for row in data : {                                      # data를 row에 담아 반복문으로 한줄식 출력
-    print(row)                                           # 화면에 출력
-}
+print(header)
 
-f.close()                                                # 출력후 csv파일 닫아주고 끝낸다.
+max_dig = -999
+max_dig_date = ''
+
+for row in data :                           # data 값을 row에 치환하여 반복
+    if row[4] != '' :
+        if max_dig < float(row[4]) :
+            max_dig_date = row[0]
+            max_dig = float(row[4])
+
+    print(row)                              # 출력
+f.close()                                   # 파일 모두 출력후 파일닫기                                             # 출력후 csv파일 닫아주고 끝낸다.
+
+print("max dig : ", max_dig, "date 한글: " + max_dig_date)
+
+plt.plot([1, 2, 3, 4], [30, 10, 40, 20])
+plt.show()
  
