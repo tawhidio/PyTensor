c37ac60aee71aad7fd239329a9e90d8370b2588b
sidcode <sidcode@gmail.com>
2020-12-24 16:29:46 +0900

    딥러닝_기초_공부시간과_시험점수의관계

--- MathToPython/연립방정식001.py
+++ MathToPython/연립방정식001.py
@@ -1,12 +1,15 @@
 from sympy import Symbol, solve
 
 x = Symbol('x')
+y = Symbol('y')
 
-# eq001 = 2 * x - 4 * x - 9 - 3
-# 2x + 3 = 4x - 9
-# 
+## 2x + 3 = 4x - 9# 
+
 eq001 = 2 * x - 4 * x - (-9 - 3)
 
 result = solve(eq001)
-print(result)
+print("result => ", result)
 
+eq002 = y - 10 * x + 1234
+result2 = solve(eq002)
+print("reuslt2 => ", eq002)
